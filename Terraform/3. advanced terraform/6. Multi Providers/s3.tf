dda1fb4b3d23be2b486556fa5ea7e81c210f4e12
Eeunchong <dldmschd97@gmail.com>
2024-02-28 17:45:07 +0900

    TEST0228

---
+++ Terraform/3. advanced terraform/6. Multi Providers/s3.tf
@@ -0,0 +1,19 @@
+## S3 생성 ##
+# resource "aws_s3_bucket" "apne2" {
+#   force_destroy = true
+# }
+
+# resource "aws_s3_bucket" "use1" {
+#   provider      = aws.use1
+#   force_destroy = true
+# }
+
+## vpc 생성 ##
+resource "aws_vpc" "apne2" {
+  cidr_block = "10.3.0.0/16"
+}
+
+resource "aws_vpc" "use1" {
+  cidr_block = "10.3.0.0/16"
+  provider   = aws.use1
+}
